1165f9049f9768076327cab1a15e39b292c69d45
Basile Jeannot <bajeanno@student.42lyon.fr>
2025-02-18 12:05:02 +0100

    implemented Display trait for TaskServer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 //Author Basile Jeannot bajeanno 42Lyon Member
 
+use std::fmt::Display;
+
 #[derive(Debug)]
 struct Task {
 	id: u32,
@@ -15,12 +17,6 @@ impl Task {
 	}
 }
 
-// impl Display for Task {
-// 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-// 		write!(f, "{}\t{}", self.id, self.name)
-// 	}
-// }
-
 struct TaskServer {
 	tasks: Vec<Task>,
 }
@@ -29,13 +25,22 @@ impl TaskServer {
 	fn new() -> Self {
 		Self { tasks: Vec::new() }
 	}
-
+	
 	fn run(&self) {
 		loop {};
 	}
-
-	fn create_task(&mut self, task_name: &str){
+	
+	fn create_task(&mut self, task_name: &str) {
 		self.tasks.push(Task::new(self.tasks.len() as u32, task_name));
+	}
+}
+
+impl Display for TaskServer {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		let tasks: Vec<String> = self.tasks.iter()
+		.map(|task| format!("{}\t{}", task.id, task.name))
+		.collect();
+		write!(f, "{}", tasks.join("\n"))
 	}
 }
 
@@ -44,5 +49,6 @@ fn main() {
     let mut server = TaskServer::new();
     server.create_task("task 0");
     server.create_task("task 1");
+	println!("{}", server);
     server.run();
 }
