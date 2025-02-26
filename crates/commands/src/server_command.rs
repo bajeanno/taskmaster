5dfb68f9a6bf6d38266de5242580ae54606875a1
bolide <bajeanno@student.42lyon.fr>
2025-02-26 18:41:46 +0100

    added some server commands

--- crates/commands/src/server_command.rs
+++ crates/commands/src/server_command.rs
@@ -3,4 +3,9 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub enum ServerCommand {
     ListTasks,
+    StartProgram(String),
+    StopProgram(String),
+    RestartProgram(String),
+    ReloadConfigFile,
+    StopDaemon,
 }
