ed75d349cdb854be41eceb1255799601340f4b11
Basile Jeannot <bajeanno@student.42lyon.fr>
2025-02-27 17:57:30 +0100

    Rename commands enums
    * rename command files
    
    * renamed all commands to command except crate name

--- taskmaster/src/task_server/client_handler/commands/list_tasks.rs
+++ taskmaster/src/task_server/client_handler/commands/list_tasks.rs
@@ -1,4 +1,4 @@
-use commands::ClientCommands;
+use commands::ClientCommand;
 use tokio::io::{AsyncRead, AsyncWrite};
 
 use crate::task_server::{
@@ -15,7 +15,7 @@ where
         eprintln!("Client {} requested ListTasks", self.client_id);
 
         let task_list = self.task_manager.lock().await.list_tasks();
-        let response = ClientCommands::TaskList(task_list);
+        let response = ClientCommand::TaskList(task_list);
 
         eprintln!("Client {} ListTasks response: {response:?}", self.client_id);
         self.write_frame(&response).await
@@ -24,7 +24,7 @@ where
 
 #[cfg(test)]
 mod test {
-    use commands::{ClientCommands, ServerCommands};
+    use commands::{ClientCommand, ServerCommand};
 
     use crate::task_server::{client_handler, task_manager::MockTaskManagerTrait};
 
@@ -38,14 +38,11 @@ mod test {
 
         let (mut client, server) = client_handler::test_utils::setup_test(mock_task_manager).await;
 
-        client
-            .write_frame(&ServerCommands::ListTasks)
-            .await
-            .unwrap();
+        client.write_frame(&ServerCommand::ListTasks).await.unwrap();
         let frame = client.read_frame().await.unwrap();
         assert_eq!(
             frame,
-            Some(ClientCommands::TaskList("List of tasks".to_string()))
+            Some(ClientCommand::TaskList("List of tasks".to_string()))
         );
 
         server.check_errors(client).await;
