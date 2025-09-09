dfd413b969e5eaaaa74519047968156011d3f7ac
bolide <bajeanno@student.42lyon.fr>
2025-09-09 16:25:12 +0200

    fixed conflict in server commands

--- crates/commands/src/server_command.rs
+++ crates/commands/src/server_command.rs
@@ -3,12 +3,9 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub enum ServerCommand {
     ListTasks,
-<<<<<<< HEAD
     StartProgram(String),
     StopProgram(String),
     RestartProgram(String),
     ReloadConfigFile,
     StopDaemon,
-=======
->>>>>>> main
 }
