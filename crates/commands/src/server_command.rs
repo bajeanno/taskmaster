caf10ba4223d316d520614bf9898577357ef1317
bolide <bajeanno@student.42lyon.fr>
2025-10-14 15:24:10 +0200

    removed ServerCommand enum except ListTask to comply with main branch

--- crates/commands/src/server_command.rs
+++ crates/commands/src/server_command.rs
@@ -3,9 +3,4 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub enum ServerCommand {
     ListTasks,
-    StartProgram(String),
-    StopProgram(String),
-    RestartProgram(String),
-    ReloadConfigFile,
-    StopDaemon,
 }
