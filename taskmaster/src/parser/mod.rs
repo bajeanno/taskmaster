8e65f2e975b33c581a889710a21246c5d3e89f8c
bolide <bajeanno@student.42lyon.fr>
2025-12-04 04:00:37 +0100

    ran cargo fmt

--- taskmaster/src/parser/mod.rs
+++ taskmaster/src/parser/mod.rs
@@ -4,23 +4,35 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ParseError {
-    #[error("Error opening taskmaster config file: {0}\n\
-     Consider making a reload request after creating one")]
+    #[error(
+        "Error opening taskmaster config file: {0}\n\
+     Consider making a reload request after creating one"
+    )]
     OpeningFile(#[from] std::io::Error),
-    #[error("Error parsing taskmaster config file: {0}\n\
-     Consider making a reload request after fixing the issue")]
+    #[error(
+        "Error parsing taskmaster config file: {0}\n\
+     Consider making a reload request after fixing the issue"
+    )]
     InvalidYaml(#[from] serde_yaml::Error),
-    #[error("Error parsing taskmaster config file: invalid stopsignal {0} for program \
+    #[error(
+        "Error parsing taskmaster config file: invalid stopsignal {0} for program \
      {1}\n\
-     Consider making a reload request after fixing the issue")]
+     Consider making a reload request after fixing the issue"
+    )]
     InvalidUmask(String, String),
-    #[error("Error parsing taskmaster config file: {0} for program {1}\n\
-     Consider making a reload request after fixing the issue")]
+    #[error(
+        "Error parsing taskmaster config file: {0} for program {1}\n\
+     Consider making a reload request after fixing the issue"
+    )]
     InvalidSignal(String, String),
-    #[error("Error parsing taskmaster config file: Empty command for program {0}\n\
-     Consider making a reload request after fixing the issue")]
+    #[error(
+        "Error parsing taskmaster config file: Empty command for program {0}\n\
+     Consider making a reload request after fixing the issue"
+    )]
     EmptyCommand(String),
-    #[error("Error parsing taskmaster config file: {0}\n\
-     Consider making a reload request after fixing the issue")]
+    #[error(
+        "Error parsing taskmaster config file: {0}\n\
+     Consider making a reload request after fixing the issue"
+    )]
     CommandError(#[from] shell_words::ParseError),
 }
