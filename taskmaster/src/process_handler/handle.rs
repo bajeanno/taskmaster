bcd8328970716f380f74f95ad1edcfc23a158e00
bolide <bajeanno@student.42lyon.fr>
2026-01-18 05:29:50 +0100

    style: ran cargo fmt

--- taskmaster/src/process_handler/handle.rs
+++ taskmaster/src/process_handler/handle.rs
@@ -1,7 +1,4 @@
-use crate::{
-    client_handler::ClientId,
-    process_handler::routine::{LogReceiver, StatusReceiver},
-};
+use crate::process_handler::routine::{LogReceiver, StatusReceiver};
 use derive_getters::Getters;
 use tokio::task::JoinHandle as TokioJoinHandle;
 
