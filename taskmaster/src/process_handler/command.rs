18378cc19a31844d4fce94554538a5db70e1030d
Basile Jeannot <bajeanno@student.42lyon.fr>
2026-06-01 20:44:36 +0200

    refactor: passing a reference instead of cloning command
    Signed-off-by: Basile Jeannot <bajeanno@student.42lyon.fr>

--- taskmaster/src/process_handler/command.rs
+++ taskmaster/src/process_handler/command.rs
@@ -4,7 +4,7 @@ use tokio::process::Command;
 pub(super) fn create_command(config: &ProgramConfig) -> Command {
     let mut command = Command::new("bash");
     command.arg("-c");
-    command.arg(config.cmd.clone());
+    command.arg(&config.cmd);
 
     if *config.clear_env() {
         command.env_clear();
