d3b6e39b8f1020f84735bee40eeb2a815c131ab4
Basile Jeannot <bajeanno@student.42lyon.fr>
2026-09-11 19:39:14 +0200

    fix: refixed clippy error

--- taskmaster/src/config/serialize.rs
+++ taskmaster/src/config/serialize.rs
@@ -7,7 +7,7 @@ pub fn serialize_signal<S>(signal: &Signal, serializer: S) -> Result<S::Ok, S::E
 where
     S: Serializer,
 {
-    serializer.serialize_str(&signal.to_string())
+    serializer.serialize_str(signal.as_ref())
 }
 
 pub fn serialize_umask<S>(umask: &mode_t, serializer: S) -> Result<S::Ok, S::Error>
