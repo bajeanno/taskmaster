788879d685a764ee7ad4525c426e686ed383e67a
Basile Jeannot <bajeanno@student.42lyon.fr>
2026-09-14 01:24:56 +0200

    style: ran cargo fmt

--- taskmaster/src/config/deserialize.rs
+++ taskmaster/src/config/deserialize.rs
@@ -32,12 +32,9 @@ where
         ));
     }
     let umask_str = umask_str.trim_start_matches("0o").to_string();
-    if umask_str.contains(|c: char| {
-        !c.is_ascii_digit()
-    }) {
+    if umask_str.contains(|c: char| !c.is_ascii_digit()) {
         return Err(serde::de::Error::custom(
-            "Failed to parse umask: umask contains non-digit characters"
-                .to_string(),
+            "Failed to parse umask: umask contains non-digit characters".to_string(),
         ));
     }
     let umask = mode_t::from_str_radix(umask_str.as_str(), 8).map_err(|err| {
